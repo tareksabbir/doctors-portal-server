db55c830a6a5dfce5a9fbc3a3d5e700ff9ccdbd5
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-19 09:12:58 +0600

    booking Data post

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4fjcw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +16,8 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -26,15 +26,31 @@ async function run() {
             res.send(services);
         })
 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { doctor: booking.doctor, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
 
+        /********* 
+        * Api name
+        * app.get('/booking') // get all booking
+        * app.get('/booking/:id') // get one or specifice booking
+        * app.post('/booking') // add new booking
+        * app.patch('/booking/:id') // update booking
+        * app.delete('/booking') // delete one booking
+        * ***************/
 
     } finally {
 
     }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
     res.send('Hello From doctors hub server!')
