91f6d1df2313716ada121f04df0248b29ae03d10
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-21 09:11:57 +0600

    admin dashboard backend

--- index.js
+++ index.js
@@ -51,14 +51,30 @@ async function run() {
             res.send(users);
         })
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+
 
         })
 
@@ -102,9 +118,7 @@ async function run() {
                 const bookings = await bookingCollection.find(query).toArray();
                 return res.send(bookings);
             }
-            else {
-                return res.status(403).send({ message: 'Forbidden Access' })
-            }
+
 
         })
 
